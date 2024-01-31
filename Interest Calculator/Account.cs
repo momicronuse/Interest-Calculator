690d0a05374343e6f55b78e5312ecbad12679e36
momicronuse <101482327+momicronuse@users.noreply.github.com>
2024-01-31 16:24:30 +0000

    Generated project files
    Starting point; empty EntryPoint and Account class

---
+++ Interest Calculator/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interest_Calculator
+{
+    internal class Class1
+    {
+    }
+}

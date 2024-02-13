67a4f91366f4472fef287a2d51845b19b2306d14
momicronuse <101482327+momicronuse@users.noreply.github.com>
2024-02-13 00:51:30 +0000

    New code plan
    Might be better thought through and has less inputs/outputs for simplicity. Assumes only 1 account each for savings/ISA

--- Interest Calculator/Account.cs
+++ Interest Calculator/Account.cs
@@ -1,4 +1,17 @@
-﻿
-internal class Account
+﻿internal class Account
 {
+    double interestRate;
+    double currentBalance;
+    bool isTaxable;
+  
+
+    public Account(double interestRate, double currentBalance, bool isTaxable)
+    {
+        this.interestRate = interestRate;
+        this.currentBalance = currentBalance;
+        this.isTaxable = isTaxable;
+
+      
+    }
+    
 }
--- Interest Calculator/README.md
+++ Interest Calculator/README.md
@@ -1,2 +1,4 @@
 # Interest Calculator
  Takes an input of multiple accounts and calculates interest earned / tax paid across all accounts
+ Account information is partially input via code in EntryPoint.cs
+
